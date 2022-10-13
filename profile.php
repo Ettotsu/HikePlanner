66fcef291cd76b73775a33a6019dfb1e8de12653
Julien <babacooldu95@gmail.com>
2022-10-13 15:21:26 +0200

    Mise A Jour 1 (MAJ1)

---
+++ profile.php
@@ -0,0 +1,20 @@
+<html>
+    <body>
+        <?php
+            session_start();
+
+            $bdd = new PDO("mysql:host=localhost;dbname=projet_if3;charset=utf8", "root", "");
+            $req = $bdd->prepare("SELECT * FROM account WHERE id = ?");
+            $req->execute([$_SESSION['id_account']]);
+
+            $data = $req->fetch();
+
+        echo "<label for='email'>Your Email</label>
+            <input id='email' name='email' type='text' value=".$data["email"]."><br>";
+        echo "<label for='username'>Username</label>
+        <input id='username' name='username' type='text' value=".$data["username"]."><br>";
+        echo "<label for='level'>Your Level</label>
+        <input id='level' name='level' type='text' value=".$data["level"].">";
+        ?>        
+    </body>
+</html>
