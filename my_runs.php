bd3fbc6151180734245be197d6bd00f877a0c970
Julien <babacooldu95@gmail.com>
2022-10-26 17:25:15 +0200

    Mise A JOUR (MAJ2.0)

---
+++ my_runs.php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+        <p>
+        beubeu
+        </p>
+    <?php
+
+
+    ?>
+    </body>
+</html>
--- save.php
+++ save.php
@@ -1,39 +1,71 @@
 <html>
     <?php
+        session_start();
+
         $lat = $_GET["lat"];
         $long = $_GET["lng"];
-        $i = 0;
+        $new = 1;
 
         $bdd = new PDO("mysql:host=localhost;dbname=projet_if3;charset=utf8", "root", "");
 
-        $i = count($lat);
+        $size = count($lat);
 
-        echo $i;
+        echo "taille :".$size."<br>";
 
-        $req = $bdd->prepare("SELECT id_run FROM waypoints HAVING MAX(order_waypoint)  =  ?");
-        $req->execute([$i]);
+        $req = $bdd->prepare("SELECT id_run, MAX(order_waypoint) FROM waypoints GROUP BY id_run HAVING MAX(order_waypoint) = ?");
+        $req->execute([$size]);
 
         foreach($req as $value) {
-            
-            echo $value["id_run"]."<br>"; 
+            echo "id_run :".$value["id_run"]."<br>";
+
+            $i = 0;
+            $new = 0;
+
+            $req1 = $bdd->prepare("SELECT latitude, longitude FROM waypoints WHERE id_run = ?");
+            $req1->execute([$value["id_run"]]);
+
+            foreach($req1 as $coord) {
+                echo "lat : ".$lat[$i]."<br>";
+                echo "lng : ".$long[$i]."<br>";
+                echo "coord lat : ".$coord["latitude"]."<br>";
+                echo "coord lng : ".$coord["longitude"]."<br><br>";
+
+                if($lat[$i] != $coord["latitude"] and $long[$i] != $coord["longitude"]) {
+                    $new = 1;
+                    break;
+                }
+                $i++;
+            }
+
+            if($new == 0) {
+                $id_run = $value["id_run"];
+                echo "id_run :".$id_run."<br>";
+                break;
+            }
+
         }
 
-        // $req = $bdd->prepare("INSERT INTO run (data) VALUES ('4')");
-        // $req->execute();
-
-        // $req = $bdd->prepare("SELECT MAX(id_run) FROM run");
-        // $req->execute();
-        // $id_run = $req->fetch();
+        echo "nouveau :".$new."<br>";
         
-        // echo $id_run["MAX(id_run)"];
+        if($new == 1) {
 
-        // for($i = 0; $lat[$i] != NULL; $i++) {
+            $req = $bdd->prepare("INSERT INTO run (data) VALUES ('4')");
+            $req->execute();
 
-        //     $req = $bdd->prepare("INSERT INTO waypoints (id_run, order_waypoint, latitude, longitude) VALUES (?,?,?,?)");
-        //     $req->execute([$id_run["MAX(id_run)"], $i + 1, $lat[$i], $long[$i]]);
-        // }
+            $req = $bdd->prepare("SELECT MAX(id_run) FROM run");
+            $req->execute();
+            $id_run = $req->fetch()["MAX(id_run)"];
 
-        // echo "lat : ".$lat[0]."<br>";
-        // echo "lng : ".$long[0];
+            for($j = 0; $lat[$j] != NULL; $j++) {
+
+                $req = $bdd->prepare("INSERT INTO waypoints (id_run, order_waypoint, latitude, longitude) VALUES (?,?,?,?)");
+                $req->execute([$id_run, $j + 1, $lat[$j], $long[$j]]);
+            }
+        }
+
+        $req = $bdd->prepare("INSERT INTO run_saved (id_account, id_run) VALUES (?, ?)");
+        $req->execute([$_SESSION['id_account'], $id_run]);
+        
+        header("Location: my_runs.php");                
     ?>
 </html>
