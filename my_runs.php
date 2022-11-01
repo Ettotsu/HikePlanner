0dc5d8e2d7aa8ab02e512cefff3eca8c32b72c51
Julien <babacooldu95@gmail.com>
2022-11-01 17:28:44 +0100

    Mise A Jour 1.10 (MAJ1.10)

--- my_runs.php
+++ my_runs.php
@@ -1,11 +1,29 @@
 <html>
     <body>
         <p>
-        beubeu
+            beubeu
         </p>
-    <?php
+        <p>
+        
+            <?php
+                session_start();
 
+                $bdd = new PDO("mysql:host=localhost;dbname=projet_if3;charset=utf8", "root", "");
 
-    ?>
+                $req = $bdd->prepare("SELECT id_run FROM run_saved WHERE id_account = ?");
+                $req->execute([$_SESSION['id_account']]);
+
+                foreach($req as $value) {
+                    echo "run : ".$value["id_run"]."<br>";
+
+            ?>
+            <a href="">edit</a>
+            <br><br>
+
+            <?php
+                }
+
+            ?>
+        </p>
     </body>
 </html>
