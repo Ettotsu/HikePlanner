0cf553264a03904588f11589dc8e6921b4d9e7ca
Ettotsu <eliott.speyser@free.fr>
2022-11-02 17:10:49 +0100

    Update page register

--- login_1.php
+++ login_1.php
@@ -1,8 +1,8 @@
 <html>
-    <body>
-    <link rel="stylesheet" type="text/css" href="projet_css/login.css"/>
+    <head><title>Hikeplanner - Login</title></head>
 
-    <head><title>Hikeplanner Login</title></head>
+    <body>
+    <link rel="stylesheet" type="text/css" href="./projet_css/login.css"/>
 
         <page>
                     <!-- <?php 
@@ -11,17 +11,16 @@
             }
         ?> -->
             <div class="logo">
-                <img src="projet_css/HikePlanner.png"/>
+                <img src="./projet_css/HikePlanner.png"/>
             </div>
-            <break></break>
             <div class="formulaire">
                 <form method="POST" action="login_2.php">
 
-                    <label for="username">Username : </label>
+                    <label for="username">Username :</label>
                     <br>
                     <input id="username" name="username" type="text"/>
                     <br>
-                    <label for="password">Password : </label>
+                    <label for="password">Password :</label>
                     <br>
                     <input id="password" name="password" type="password"/>
                     <br>
@@ -29,7 +28,6 @@
                     <a class="sign-up" href="create_account_1.php">Sign up</a>
                 </form>
             </div>
-            <break></break>
             <footer>
                 <a class="credit" href="https://www.pexels.com/photo/scenic-photo-of-forest-with-sunlight-1757363/" target="blank"><strong>Background credit</strong></a>
             </footer>
