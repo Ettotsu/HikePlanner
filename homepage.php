d4c80c9f5b10a955d812756a07eccfe8ab2c57f0
Ettotsu <eliott.speyser@free.fr>
2022-11-13 14:50:44 +0100

    ;

--- homepage.php
+++ homepage.php
@@ -154,7 +154,7 @@
                     </div>
 
                     <div class="table_run">
-                        <h3> <?php echo "distance : ".$parkour["distance"]."km time : ".$parkour["estimated_time"]; ?> </h3>
+                        <h3> <?php echo "distance : ".$parkour["distance"]."km ; time : ".$parkour["estimated_time"]; ?> </h3>
                         <table>
                             <tr>
                                 <th></th>
